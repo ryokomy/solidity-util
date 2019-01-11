e10d55e33ec3f80384e362c4ae82885abd75cbf3
Ryo Komiyama <komikomi.mikomiko@gmail.com>
2019-01-11 21:05:36 +0900

    Fix modifier in Address.toBytes

--- lib/Addresses.sol
+++ lib/Addresses.sol
@@ -5,7 +5,11 @@
  */
 
 library Addresses {
-   function toBytes(address a) constant returns (bytes b){
+   function toBytes(address a)
+      internal
+      pure 
+      returns (bytes b)
+   {
       assembly {
          let m := mload(0x40)
          mstore(add(m, 20), xor(0x140000000000000000000000000000000000000000, a))
@@ -14,4 +18,3 @@ library Addresses {
       }
    }
 }
-
